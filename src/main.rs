8748d870259d415e4b913f890cdc274277661cad
magic_rb <richard@numtide.com>
2025-12-02 21:01:13 +0100

    Remove some cleanups as input was improved
    Signed-off-by: magic_rb <richard@numtide.com>

--- src/main.rs
+++ src/main.rs
@@ -221,18 +221,15 @@ enum ZfsAction {
 
 #[derive(Deserialize, Serialize)]
 struct ZfsSpecDataset {
-    name: String,
     properties: HashMap<String, PropertyValue>,
 }
 
 impl ZfsSpecDataset {
-    pub fn new<S1, S2>(name: S1, properties: HashMap<S2, PropertyValue>) -> ZfsSpecDataset
+    pub fn new<S>(properties: HashMap<S, PropertyValue>) -> ZfsSpecDataset
     where
-        S1: AsRef<str>,
-        S2: AsRef<str>,
+        S: AsRef<str>,
     {
         ZfsSpecDataset {
-            name: name.as_ref().to_owned(),
             properties: properties
                 .into_iter()
                 .map(|(k, v)| (k.as_ref().to_owned(), v))
@@ -243,7 +240,7 @@ impl ZfsSpecDataset {
 
 #[derive(Deserialize, Serialize)]
 struct ZfsSpec {
-    datasets: Vec<ZfsSpecDataset>,
+    datasets: HashMap<String, ZfsSpecDataset>,
 }
 
 impl ZfsSpec {
@@ -269,7 +266,7 @@ impl ActionSet {
                     output.extend(
                         properties
                             .iter()
-                            .map(|(name, value)| format!("-o {}={}", name, value.to_string())),
+                            .map(|(name, value)| format!("-o{}={}", name, value.to_string())),
                     );
                     output.push(name.to_owned());
                     output
@@ -352,13 +349,16 @@ impl VecActionProducer {
                             )
                         }
 
-                        edited_properties
-                            .into_iter()
-                            .map(|(property_name, property_value)| ZfsAction::SetProperties {
+                        if !edited_properties.is_empty() {
+                            [ZfsAction::SetProperties {
                                 dataset: name.clone(),
-                                properties: HashMap::from([(property_name, property_value)]),
-                            })
+                                properties: edited_properties,
+                            }]
+                            .into_iter()
                             .collect()
+                        } else {
+                            [].into_iter().collect()
+                        }
                     } else {
                         log::trace!("new dateset {}, keeping", name);
                         known_datasets.insert(name.clone(), properties.clone());
@@ -370,142 +370,12 @@ impl VecActionProducer {
             .collect::<Vec<_>>()
     }
 
-    fn cleanup_shadowed_properties(actions: Vec<ZfsAction>) -> Vec<ZfsAction> {
-        let mut set_properties = HashSet::new();
-
-        actions
-            .into_iter()
-            .rev()
-            .filter_map(|action| match action {
-                ZfsAction::CreateDataset {
-                    name: dataset,
-                    properties,
-                } => Some(ZfsAction::CreateDataset {
-                    properties: properties
-                        .into_iter()
-                        .filter(|(name, value)| {
-                            if set_properties.contains(&(dataset.clone(), name.clone())) {
-                                log::trace!(
-                                    "dataset {} discard shadowed property {}={}",
-                                    dataset,
-                                    name,
-                                    value.to_string()
-                                );
-                                false
-                            } else {
-                                log::trace!(
-                                    "dataset {} keep new property {}={}",
-                                    dataset,
-                                    name,
-                                    value.to_string()
-                                );
-                                set_properties.insert((dataset.clone(), name.clone()));
-                                true
-                            }
-                        })
-                        .collect(),
-                    name: dataset,
-                }),
-                ZfsAction::SetProperties {
-                    dataset,
-                    properties,
-                } => Some(ZfsAction::SetProperties {
-                    properties: properties
-                        .into_iter()
-                        .filter_map(|(name, value)| {
-                            if set_properties.contains(&(dataset.clone(), name.clone())) {
-                                log::trace!(
-                                    "dataset {} shadowed property {}={}",
-                                    dataset,
-                                    name,
-                                    value.to_string()
-                                );
-                                None
-                            } else {
-                                log::trace!("dataset {} new property {}", dataset, name);
-                                set_properties.insert((dataset.clone(), name.clone()));
-                                Some((name, value))
-                            }
-                        })
-                        .collect(),
-                    dataset,
-                }),
-            })
-            .collect::<Vec<_>>()
-            .into_iter()
-            .rev()
-            .collect()
-    }
-
-    fn cleanup_fold_properties_into_creates(actions: Vec<ZfsAction>) -> Vec<ZfsAction> {
-        let mut creates: HashMap<String, HashMap<String, PropertyValue>> = actions
-            .iter()
-            .filter_map(|action| match action {
-                ZfsAction::CreateDataset { name, properties } => {
-                    Some((name.to_owned(), properties.to_owned()))
-                }
-                ZfsAction::SetProperties {
-                    dataset,
-                    properties,
-                } => None,
-            })
-            .collect();
-
-        actions
-            .into_iter()
-            .filter_map(|action| match action {
-                ZfsAction::CreateDataset { .. } => Some(action),
-                ZfsAction::SetProperties {
-                    dataset,
-                    properties,
-                } => {
-                    if creates.contains_key(&dataset) {
-                        creates.entry(dataset).and_modify(|existing_properties| {
-                            existing_properties.extend(properties)
-                        });
-
-                        None
-                    } else {
-                        Some(ZfsAction::SetProperties {
-                            dataset,
-                            properties,
-                        })
-                    }
-                }
-            })
-            .collect::<Vec<_>>()
-            .into_iter()
-            .map(|action| match action {
-                ZfsAction::CreateDataset {
-                    name,
-                    properties: _properties,
-                } => ZfsAction::CreateDataset {
-                    properties: creates.remove(&name).unwrap(),
-                    name,
-                },
-                ZfsAction::SetProperties {
-                    dataset,
-                    properties,
-                } => ZfsAction::SetProperties {
-                    dataset,
-                    properties,
-                },
-            })
-            .collect()
-    }
-
     fn cleanup(&mut self) {
-        self.actions = vec![
-            Self::cleanup_multiple_creates,
-            Self::cleanup_shadowed_properties,
-            Self::cleanup_fold_properties_into_creates,
-        ]
-        .into_iter()
-        .fold(std::mem::take(&mut self.actions), |acc, f| f(acc))
+        self.actions = Self::cleanup_multiple_creates(std::mem::take(&mut self.actions));
     }
 
     fn finalize(mut self) -> (ActionSet, Vec<String>) {
-        // self.cleanup();
+        self.cleanup();
         (ActionSet(self.actions), self.errors)
     }
 }
@@ -529,9 +399,12 @@ fn eval_spec<AP>(action_producer: &mut AP, state: &ZfsListOutput, spec: &ZfsSpec
 where
     AP: ActionProducer,
 {
-    for dataset in spec.datasets.iter() {
-        if let Some(dataset_state) = state.get_dataset(&dataset.name) {
-            log::trace!("dataset {} already exists", dataset.name);
+    let mut datasets = spec.datasets.iter().collect::<Vec<_>>();
+    datasets.sort_by_key(|(key, _)| key.len());
+
+    for (name, dataset) in datasets {
+        if let Some(dataset_state) = state.get_dataset(name) {
+            log::trace!("dataset {} already exists", name);
 
             let mut properties = HashMap::new();
 
@@ -544,7 +417,7 @@ where
                             if property_state.value != *value {
                                 log::trace!(
                                     "dataset {} property {} set to {}, modify to {}",
-                                    dataset.name,
+                                    name,
                                     property,
                                     property_state.value.to_string(),
                                     value.to_string()
@@ -553,28 +426,24 @@ where
                             } else {
                                 log::trace!(
                                     "dataset {} property {} already set to {}, skip",
-                                    dataset.name,
+                                    name,
                                     property,
                                     value.to_string()
                                 );
                             }
                         }
                         _ => {
-                            log::trace!(
-                                "dataset {} property {} not normal, error",
-                                dataset.name,
-                                property,
-                            );
+                            log::trace!("dataset {} property {} not normal, error", name, property,);
                             action_producer.produce_error(format!(
                                 "cannot set property {} of dataset {} because source is {:?}",
-                                property, dataset.name, property_state.source
+                                property, name, property_state.source
                             ))
                         }
                     }
                 } else {
                     log::trace!(
                         "dataset {} property {} not set, set to {}",
-                        dataset.name,
+                        name,
                         property,
                         value.to_string(),
                     );
@@ -582,12 +451,14 @@ where
                 }
             }
 
-            action_producer.produce_action(ZfsAction::SetProperties {
-                dataset: dataset.name.to_owned(),
-                properties,
-            })
+            if !properties.is_empty() {
+                action_producer.produce_action(ZfsAction::SetProperties {
+                    dataset: name.to_owned(),
+                    properties,
+                })
+            }
         } else {
-            log::trace!("prepare dataset {}", dataset.name);
+            log::trace!("prepare dataset {}", name);
 
             struct PrefixPaths<'a>(&'a str, MatchIndices<'a, char>);
 
@@ -605,7 +476,7 @@ where
                 }
             }
 
-            for dataset_part in PrefixPaths::new(&dataset.name) {
+            for dataset_part in PrefixPaths::new(&name) {
                 if state.get_dataset(dataset_part).is_none() {
                     log::trace!("create parent dataset {}", dataset_part);
                     action_producer.produce_action(ZfsAction::CreateDataset {
@@ -616,7 +487,7 @@ where
             }
             log::trace!(
                 "create dataset {} with properties {}",
-                dataset.name,
+                name,
                 dataset
                     .properties
                     .iter()
@@ -625,7 +496,7 @@ where
                     .join(" ")
             );
             action_producer.produce_action(ZfsAction::CreateDataset {
-                name: dataset.name.to_owned(),
+                name: name.to_owned(),
                 properties: dataset.properties.to_owned(),
             })
         }
@@ -721,14 +592,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         ZfsSpec::from_reader(file)?
     };
 
+    let mut ap = VecActionProducer::new();
+
+    eval_spec(&mut ap, &zfs_list_output, &zfs_spec);
+
+    let (actions, errors) = ap.finalize();
+    let commands = actions.to_commands();
+
+    for error in errors {
+        log::error!("{}", error)
+    }
+
     match cli.command {
         Commands::Plan { output } => {
-            let mut ap = VecActionProducer::new();
-
-            eval_spec(&mut ap, &zfs_list_output, &zfs_spec);
-
-            let commands = ap.finalize().0.to_commands();
-
             if let Some(output) = output {
                 let mut output = File::create(output)?;
 
@@ -743,6 +619,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             Ok(())
         }
-        Commands::Apply => todo!(),
+        Commands::Apply => {
+            for command in commands {
+                println!("+ {}", &command.join(" "));
+                Command::new(&command[0]).args(&command[1..]).status()?;
+            }
+
+            Ok(())
+        }
     }
 }
