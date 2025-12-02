fefebe2c6b22ee5b8e7918c8078ef83472d043bf
magic_rb <richard@numtide.com>
2025-12-02 21:02:38 +0100

    Add `get_binary` script
    Signed-off-by: magic_rb <richard@numtide.com>

--- dev-shells/default.nix
+++ dev-shells/default.nix
@@ -6,6 +6,19 @@
         rust-analyzer
         cargo
         rustfmt
+        (writeShellApplication {
+          name = "get_binary";
+          runtimeInputs = [
+            jq
+          ];
+          text = ''
+            TARGET_DIR="$(cargo metadata --format-version 1 --no-deps | jq -r '.target_directory')"
+            BIN_NAME="$(cargo metadata --format-version 1 --no-deps | jq -r '.packages[].targets[] | select( .kind | map(. == "bin") | any ) | .name')"
+            BINARY_PATH="$TARGET_DIR/debug/$BIN_NAME"
+
+            echo "$BINARY_PATH"
+          '';
+        })
       ];
     };
   };
